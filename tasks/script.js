a1bab1531ead28867ef91276d9003a844e8c0461
RafalNicki <Nicol79@op.pl>
2023-09-28 12:03:25 +0200

    update js

--- tasks/script.js
+++ tasks/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://tasks.nicol79.repl.co/v1/tasks';
+  var apiRoot = 'https://nickirafal-979ec63c1611.herokuapp.com/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
