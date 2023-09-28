1e428987c41a914ee39bfcc3f4c31e6dc90712aa
RafalNicki <Nicol79@op.pl>
2023-09-28 14:27:19 +0200

    update js

--- sudoku/script.js
+++ sudoku/script.js
@@ -1,4 +1,4 @@
-app.use((req, res, next) => {
+$app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Expose-Headers', '*')
