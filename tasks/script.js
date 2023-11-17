7f043e285279bd5c69d070bec5d1bed1bd35fc21
RafalNicki <Nicol79@op.pl>
2023-11-17 15:03:45 +0100

    update script.js

--- tasks/script.js
+++ tasks/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://nickirafal-979ec63c1611.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://api.trello.com/1';
+  const trelloApiRoot = 'https://nickirafal-979ec63c1611.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
