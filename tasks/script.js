999110c2cd2d40cac1533e9c4fc40ac8bb17f246
RafalNicki <Nicol79@op.pl>
2024-01-19 19:07:22 +0100

    change server

--- tasks/script.js
+++ tasks/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://nickirafal-979ec63c1611.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://nickirafal-979ec63c1611.herokuapp.com/v1/trello';
+  const apiRoot = 'https://rafalnicki.alwaysdata.net/v1/tasks';
+  const trelloApiRoot = 'https://rafalnicki.alwaysdata.net/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
